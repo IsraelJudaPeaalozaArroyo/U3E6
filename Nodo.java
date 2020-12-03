0668ded9a7c217083c94b9055ee506c7a7851c6a
Israel Juda Peñaloza Arroyo <71736208+IsraelJudaPeaalozaArroyo@users.noreply.github.com>
2020-12-02 19:52:25 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,15 @@
+public class Nodo {
+
+    int dato;
+    Nodo siguiente;
+
+    public Nodo(int dato, Nodo n) {
+        this.dato = dato;
+        this.siguiente = n;
+    }
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
